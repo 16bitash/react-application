f3b1cccb1a1b149fcf62eb57f4ac3ad903c89a01
16bitash <entropy.ash@gmail.com>
2023-01-02 23:10:40 +0530

    Add component examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+// Evaluating Expressions in JSX
+// const name = "John Doe";
+
+// const MyComponent = () => {
+//   return <h1>My name is {name}</h1>;
+// };
+
+const MyComponent = (props) => {
+  const { name, age } = props;
+
+  return <h1>My name is {name} and my age is {age}</h1>;
+};
+
+const App = () => {
   return (
-    <h1>Heading</h1>
+    <>
+      <MyComponent name="Jane" age="40" />
+      <MyComponent name="Jhon" age="30"></MyComponent>
+    </>
   );
-}
+};
 
 export default App;
