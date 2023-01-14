6179c6dae697496b99b12014ec17042f002a82e6
16bitash <entropy.ash@gmail.com>
2023-01-14 17:29:50 +0530

    Add context example

---
+++ src/Context.js
@@ -0,0 +1,44 @@
+import { useState, createContext, useContext } from "react";
+
+const CountContext = createContext(0);
+
+const Context = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <CountContext.Provider value={{ count, setCount }}>
+        <GrandParent />
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+      </CountContext.Provider>
+    </>
+  );
+};
+
+const GrandParent = () => {
+  return <Parent />;
+};
+
+const Parent = () => {
+  const { count, setCount } = useContext(CountContext);
+
+  return (
+    <>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <Child />
+    </>
+  );
+};
+
+const Child = () => {
+  const { count } = useContext(CountContext);
+
+  return (
+    <>
+      <div>Child</div>
+      <div>{count}</div>
+    </>
+  );
+};
+
+export default Context;
