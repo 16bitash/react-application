43c5688a339a01dcbab4820eda81521c78a630d2
16bitash <entropy.ash@gmail.com>
2023-01-19 23:06:42 +0530

    Add more examples to class component

--- src/ClassComponent/Function.js
+++ src/ClassComponent/Function.js
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Function = () => {
   const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(0);
 
-  const handleIncreaseButtonClick = () => {
+  useEffect(() => {
+    console.log("function component mount");
+
+    return () => {
+      console.log("function component unmount");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("function component count change");
+  }, [count]);
+
+  const increaseCount = () => {
     setCount(count + 1);
+  };
+
+  const increaseNewCount = () => {
+    setNewCount(newCount + 1);
   };
 
   return (
     <>
       <h1>Function Component</h1>
       <div>{count}</div>
-      <button onClick={handleIncreaseButtonClick}>Increase</button>
+      <button onClick={increaseCount}>Increase</button>
+
+      <div>{newCount}</div>
+      <button onClick={increaseNewCount}>Increase</button>
     </>
   );
 };
--- src/ClassComponent/index.js
+++ src/ClassComponent/index.js
@@ -6,19 +6,52 @@ class ClassComponent extends Component {
 
     this.state = {
       count: 0,
+      newCount: 0,
+      hasError: false,
     };
   }
 
-  handleIncreaseButtonClick = () => {
+  componentDidMount() {
+    console.log("class component mount");
+  }
+
+  componentWillUnmount() {
+    console.log("class component unmount");
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.count !== this.state.count) {
+      console.log("class component count change");
+    }
+  }
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  increaseCount = () => {
     this.setState({ count: this.state.count + 1 });
   };
 
+  increaseNewCount = () => {
+    this.setState({ newCount: this.state.newCount + 1 });
+  };
+
   render() {
+    if (this.state.count === 5) {
+      throw new Error("Something went wrong");
+    }
+
     return (
       <>
         <h1>Class Component</h1>
         <div>{this.state.count}</div>
-        <button onClick={this.handleIncreaseButtonClick}>Increase</button>
+        <button onClick={this.increaseCount}>Increase</button>
+
+        <div>{this.state.newCount}</div>
+        <button onClick={this.increaseNewCount}>Increase</button>
+
+        {this.state.hasError && <div>Something went wrong</div>}
       </>
     );
   }
